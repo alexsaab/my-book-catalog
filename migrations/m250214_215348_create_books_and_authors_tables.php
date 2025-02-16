97e5da1e382d9f647e9557c16e605edea80f5b2d
a.agafonov <a.agafonov@eco-fin.eu>
2025-02-16 10:18:33 +0300

    Init git commit

---
+++ migrations/m250214_215348_create_books_and_authors_tables.php
@@ -0,0 +1,56 @@
+<?php
+
+use yii\db\Migration;
+
+class m250214_215348_create_books_and_authors_tables extends Migration
+{
+    public function safeUp()
+    {
+        $this->createTable('{{%books}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(255)->notNull(),
+            'year' => $this->integer()->notNull(),
+            'description' => $this->text(),
+            'isbn' => $this->string(20)->notNull(),
+            'photo' => $this->string(255),
+        ]);
+
+        $this->createTable('{{%authors}}', [
+            'id' => $this->primaryKey(),
+            'full_name' => $this->string(255)->notNull(),
+        ]);
+
+        $this->createTable('{{%book_authors}}', [
+            'book_id' => $this->integer()->notNull(),
+            'author_id' => $this->integer()->notNull(),
+            'PRIMARY KEY (book_id, author_id)',
+        ]);
+
+        $this->addForeignKey('fk_book_authors_book_id', '{{%book_authors}}', 'book_id', '{{%books}}', 'id', 'CASCADE', 'CASCADE');
+        $this->addForeignKey('fk_book_authors_author_id', '{{%book_authors}}', 'author_id', '{{%authors}}', 'id', 'CASCADE', 'CASCADE');
+    }
+
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk_book_authors_book_id', '{{%book_authors}}');
+        $this->dropForeignKey('fk_book_authors_author_id', '{{%book_authors}}');
+        $this->dropTable('{{%book_authors}}');
+        $this->dropTable('{{%authors}}');
+        $this->dropTable('{{%books}}');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m250214_215348_create_books_and_authors_tables cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
---
+++ models/Book.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+class Book extends ActiveRecord
+{
+    public static function tableName()
+    {
+        return 'books';
+    }
+
+    public function getAuthors()
+    {
+        return $this->hasMany(Author::className(), ['id' => 'author_id'])->viaTable('book_authors', ['book_id' => 'id']);
+    }
+}
